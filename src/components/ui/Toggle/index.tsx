648165675586311c1f7d37d60950dbae9f9bac9e
Wellington Vieira <wtvsantos@gmail.com>
2025-05-05 20:49:10 -0300

    feat:create Toggle Component ui with radix

---
+++ src/components/ui/Toggle/index.tsx
@@ -0,0 +1,24 @@
+import * as ToggleRadix from '@radix-ui/react-toggle'
+import { forwardRef } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+const Toggle = forwardRef<
+  React.ComponentRef<typeof ToggleRadix.Root>,
+  React.ComponentPropsWithoutRef<typeof ToggleRadix.Root>
+>(({ children, className, ...props }, ref) => {
+  return (
+    <ToggleRadix.Root
+      ref={ref}
+      {...props}
+      className={twMerge(`inline-flex items-center justify-center gap-2 rounded h-9 w-fit px-2 
+        min-w-9 data-[state=on]:bg-button-secondary hover:bg-slate-100/30 
+        cursor-pointer transition duration-300 ${className}`)}
+    >
+      {children}
+    </ToggleRadix.Root>
+  )
+})
+
+Toggle.displayName = ToggleRadix.Root.displayName
+
+export default Toggle
