21ccec26cf8e3413c0565a1e49741d1004d4cf15
Wellington Vieira <wtvsantos@gmail.com>
2025-05-14 17:22:11 -0300

    feat: create not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,19 @@
+import { Button } from '@/components/ui/Button'
+import { ArrowUpRight } from 'lucide-react'
+
+export default function NotFound() {
+  return (
+    <div className='w-full h-[calc(100dvh-80px)] flex flex-col justify-center items-center gap-10'>
+      <h1 className='text-4xl'>Page Not Found!</h1>
+      <span className='text-terciary/70'>You Lost?</span>
+      <Button
+        variant='border'
+        size='lg'
+        icon={ArrowUpRight}
+        iconPosition='right'
+      >
+        <span>Back</span>
+      </Button>
+    </div>
+  )
+}
